dba6fee0be4fe99e44856532e0b2464442be5907
John Hornick <jhornick@live.com>
2023-03-09 13:35:26 -0500

    custom html pin dropping added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Home from './pages/Home.tsx';
+import MapArea from './pages/MapArea.tsx';
 import { AzureMapsProvider } from 'react-azure-maps';
 
 export default function Header() {
     return (
         <AzureMapsProvider>
-            <Home />
+            <MapArea />
         </AzureMapsProvider>
     );
 };
